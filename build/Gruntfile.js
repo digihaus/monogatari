ec7858ddcf2b95468cdcee33218e746ed2e3c930
Marcio Lucca <mrclucca@gmail.com>
2015-01-06 20:40:06 -0200

    adjusting build

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,36 +1,84 @@
 module.exports = function (grunt) {
   grunt.initConfig({
 
+    pkg: grunt.file.readJSON('package.json'),
+
     requirejs: {
       compile: {
         options: {
-          baseUrl: '../',
+          baseUrl: '../src/',
           paths: {
-            requireLib: 'lib/Require',
-            lib: 'lib'
+            requireLib: '../lib/Require',
+            lib: '../lib'
           },
-          name: 'engine/GameManager',
+          name: 'Monogatari',
           include: ['requireLib'],
-          out: './temp.js'
+          out: '../dist/monogatari-<%= pkg.version %>.js'
         }
       }
     },
 
-    pkg: grunt.file.readJSON('package.json'),
-
     uglify: {
       dist: {
-        src: ['./temp.js'],
-        dest: '../monogatari-<%= pkg.version %>.min.js',
+        src: ['../dist/monogatari-<%= pkg.version %>.js'],
+        dest: '../dist/monogatari-<%= pkg.version %>.min.js',
       }
     },
 
-    clean: ['./temp.js'],
+    clean: ['temp.js'],
+
+    jshint: {
+      all: {
+        src: ['../src/**/*.js'],
+        options: {
+          bitwise: false,
+          camelcase: true,
+          curly: true,
+          eqeqeq: true,
+          es3: false,
+          forin: true,
+          freeze: true,
+          immed: true,
+          indent: 4,
+          latedef: true,
+          newcap: true,
+          noarg: true,
+          noempty: true,
+          nonew: true,
+          plusplus: true,
+          quotmark: 'single',
+          regexp: true,
+          undef: true,
+          unused: true,
+          trailing: true,
+          maxlen: 400,
+          globals: {
+            'window': true,
+            'console': true,
+            'document': true,
+            'requestAnimationFrame': true,
+            'fonts': true,
+            'createjs': true,
+            'Monogatari': true,
+            'Class': true,
+            'localStorage': true,
+            'THREE': true,
+            'Detector': true,
+            'define': true,
+            'b2World': true,
+            'b2Vec': true,
+            'b2Vec2': true,
+            'b2ContactListener': true
+          }
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default',['requirejs', 'uglify', 'clean']);
+  grunt.registerTask('default', ['requirejs', 'uglify']);
 };
