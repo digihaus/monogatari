0a423a29b6d4067c72722ffc32b05f1961952386
Marcio Lucca <mrclucca@gmail.com>
2015-01-06 20:58:53 -0200

    refactoring Timer

--- src/core/Timer.js
+++ src/core/Timer.js
@@ -1,58 +1,58 @@
 define( [ 'core/Math' ], function( _Math ) {
 
-  Timer.FRAME_RATE_60FPS: 0.016666666667, // 1.0 second / 60.0 frames
+  var Timer = function() {
+    this.time = 0;
+    this.lastTime = 0;
+    this.maxStep = 60;
+    this.cycleTime = 1e9;
+    this.lastFrameTime = 0;
+    this.frameTicks = 0;
+    this.fps = 0;
+  };
 
-  var _time = 0;
-  var _lastTime = 0;
-  var _maxStep = 60;
-  var _cycleTime = 1e9;
-  var _lastFrameTime = 0;
-  var _frameTicks = 0;
-  var _fps = 0;
+  Timer.FRAME_RATE_60FPS = 0.016666666667; // 1.0 second / 60.0 frames
 
-  return {
+  // This should be accessed ONLY on the Game Manager Update
+  Timer.prototype.tick = function() {
+    // Use Date.now() instead of new Date().getTime(), avoids one object allocation
+    var now = Date.now();
+    var delta = now - this.lastTime;
 
-    // This should be accessed ONLY on the Game Manager Update
-    tick: function() {
-      // Use Date.now() instead of new Date().getTime(), avoids one object allocation
-      var now = Date.now();
-      var delta = now - _lastTime;
-
-      if ( _time > _cycleTime ) {
-        time = 0;
-      }
-
-      _time += _Math.min( delta, _maxStep );
-      _lastTime = now;
-    },
-
-    getTime: function() {
-      if ( _time > _cycleTime ) {
-        _time = 0;
-      }
-
-      return _time;
-    },
-
-    // Returns the difference in milliseconds from the given time, to current Monogatari time
-    compare: function( time ) {
-      return ( time > _time ) ? _time + _cycleTime - time : _time - time;
-    },
-
-    getFps: function() {
-      var now = this.getTime();
-      var delta = now - _lastFrameTime;
-
-      if ( delta >= 1000 ) {
-        _fps = _frameTicks;
-        _frameTicks = 0;
-        _lastFrameTime = now;
-      }
-
-      _frameTicks++;
-      return _fps;
+    if ( this.time > this.cycleTime ) {
+      time = 0;
     }
 
-  }
+    this.time += _Math.min( delta, this.maxStep );
+    this.lastTime = now;
+  };
+
+  Timer.prototype.getTime = function() {
+    if ( this.time > this.cycleTime ) {
+      this.time = 0;
+    }
+
+    return this.time;
+  };
+
+  // Returns the difference in milliseconds from the given time, to current Monogatari time
+  Timer.prototype.compare = function( time ) {
+    return ( time > this.time ) ? this.time + this.cycleTime - time : this.time - time;
+  };
+
+  Timer.prototype.getFps = function() {
+    var now = this.getTime();
+    var delta = now - this.lastFrameTime;
+
+    if ( delta >= 1000 ) {
+      this.fps = this.frameTicks;
+      this.frameTicks = 0;
+      this.lastFrameTime = now;
+    }
+
+    this.frameTicks++;
+    return this.fps;
+  };
+
+  return Timer;
 
 } );
