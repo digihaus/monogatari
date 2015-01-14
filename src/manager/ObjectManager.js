9927e4dde7d24ea3b87d53d769a1105b00245a27
chrysthian <suisen.no.ryuu@gmail.com>
2015-01-14 10:33:52 -0200

    Refactoring Object Manager

--- src/manager/ObjectManager.js
+++ src/manager/ObjectManager.js
@@ -1,188 +1,62 @@
-define( [ 'collection/Tree',
-          'core/GameObject',
-          'component/Base',
-          'component/Node' ], function( _Tree, _GameObject, _Base, _Node ) {
+define( [ 'core/GameObject', 'collection/Tree', 'collection/Map', 'component/Base', 'component/Node' ], function( _GameObject, _Tree, _Map, _Base, _Node ) {
 
-  function MonogatariObjectManager() {
-    this._objects = new _Tree();
-    this._iterator = this._objects.iterator();
+  var ObjectManager = function() {
+    this.tree = new _Tree();
+    this.map = new _Map();
+    this.mapIterator = this.map.iterator();
+  };
+
+  ObjectManager.prototype.isGameObject = function( object ) {
+    return ( object && object.id && object.equals instanceof Function && object.update instanceof Function ) ? true : false;
   }
 
-  MonogatariObjectManager.prototype.add = function( object ) {
-    return this._objects.put( object.id, object ).value;
+  ObjectManager.prototype.put = function( object, parent ) {
+    if ( this.isGameObject( object ) ) {
+      this.tree.put( object, parent );
+      return this.map.put( object.id, object );
+    }
+    return null;
+  }
+
+  ObjectManager.prototype.get = function( id ) {
+    return this.map.get( id );
   };
 
-  MonogatariObjectManager.prototype.create = function( id, update ) {
-    var object = new _GameObject( id, update );
-    return this._objects.put( id, object ).value;
+  ObjectManager.prototype.size = function() {
+    return this.map.size();
   };
 
-  MonogatariObjectManager.prototype.get = function( id ) {
-    if ( this._objects.contains( id ) ) {
-      return this._objects.get( id ).value;
-    } else {
-      return null;
+  ObjectManager.prototype.remove = function( id ) {
+    var data = this.map.get( id );
+
+    if ( data ) {
+      this.map.remove( id );
+      this.tree.remove( data );
     }
   };
 
-  MonogatariObjectManager.prototype.getObjectIterator = function() {
-    return this._objects.iterator();
-  };
+  ObjectManager.prototype.clear = function() {
+    this.map.claer();
+    this.tree.clear();
+  }
 
-  MonogatariObjectManager.prototype.size = function() {
-    return this._objects.size();
-  };
-
-  MonogatariObjectManager.prototype.remove = function( id ) {
-    return this._objects.remove( id ).value;
-  };
-
-  MonogatariObjectManager.prototype.hasChildren = function( id ) {
-    return this._objects.hasChildren( id );
-  };
-
-  MonogatariObjectManager.prototype.isChildOf = function( id, parentId ) {
-    return this._objects.isChildOf( id, parentId );
-  };
-
-  MonogatariObjectManager.prototype.listChildrenOf = function( id ) {
-    return this._objects.listChildrenOf( id );
-  };
-
-  MonogatariObjectManager.prototype.listChildrenValuesOf = function( id ) {
-    return this._objects.listChildrenValuesOf( id );
-  };
-
-  MonogatariObjectManager.prototype.isParentOf = function( id, childId ) {
-    return this._objects.isParentOf( id, childId );
-  };
-
-  MonogatariObjectManager.prototype.connect = function( parentId, childId ) {
-    this._objects.connect( parentId, childId );
-  };
-
-  MonogatariObjectManager.prototype.disconnect = function( id ) {
-    this._objects.disconnect( id );
-  };
-
-  MonogatariObjectManager.prototype.listAncestors = function( id ) {
-    return this._objects.listAncestors( id );
-  };
-
-  MonogatariObjectManager.prototype.listSuccessors = function( id ) {
-    return this._objects.listSuccessors( id );
-  };
-
-  // array containing all game objects ids
-  MonogatariObjectManager.prototype.listIds = function() {
-    return this._objects.getKeys();
-  };
-
-  MonogatariObjectManager.prototype.toJSON = function() {};
-
-  // array containing all game objects ids with components of given type
-  MonogatariObjectManager.prototype.listIdsByComponentType = function( type ) {
-    var list = [], obj;
-
-    this._iterator.first();
-
-    while ( this._iterator.hasNext() ) {
-      obj = this._iterator.next();
-
-      if ( obj && obj.hasComponent( type ) ) {
-        list.push( obj.id );
-      }
-    }
-
-    return list;
-  };
-
-  MonogatariObjectManager.prototype.update = function() {
+  ObjectManager.prototype.update = function() {
     var obj;
 
-    this._iterator.first();
+    this.mapIterator.first();
 
-    while ( this._iterator.hasNext() ) {
-      obj = this._iterator.next();
+    while ( this.mapIterator.hasNext() ) {
+      obj = this.mapIterator.next();
       if ( obj && obj.isActive ) {
         obj.update();
-        obj.postUpdate();
       }
+      obj.postUpdate();
     }
-  };
+  }
 
-  // leaks?
-  MonogatariObjectManager.prototype.getGlobalPosition = function( id ) {
-    if ( this._objects.contains( id ) ) {
-      if ( this._objects.get( id ).parent === 'trunk' ) {
-        return this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).position;
-      } else {
-        var parentPosition = this.getGlobalPosition( this._objects.get( id ).parent );
-        var position = this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).position.clone();
-
-        position.add( parentPosition.getX(), parentPosition.getY(), parentPosition.getZ() );
-
-        return position;
-      }
-    }
-    return null;
-  };
-
-  // leaks?
-  MonogatariObjectManager.prototype.getGlobalRotation = function( id ) {
-    if ( this._objects.contains( id ) ) {
-      if ( this._objects.get( id ).parent === 'trunk' ) {
-        return this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).rotation;
-      } else {
-        var parentRotation = this.getGlobalRotation( this._objects.get( id ).parent );
-        var rotation = this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).rotation.clone();
-
-        rotation.add( parentRotation.getX(), parentRotation.getY(), parentRotation.getZ() );
-
-        return rotation;
-      }
-    }
-    return null;
-  };
-
-  // leaks?
-  MonogatariObjectManager.prototype.getGlobalScale = function( id ) {
-    if ( this._objects.contains( id ) ) {
-      if ( this._objects.get( id ).parent === 'trunk' ) {
-        return this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).scale;
-      } else {
-        var parentScale = this.getGlobalScale( this._objects.get( id ).parent );
-        var scale = this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).scale.clone();
-
-        scale.add( parentScale.getX(), parentScale.getY(), parentScale.getZ() );
-
-        return scale;
-      }
-    }
-    return null;
-  };
-
-  // leaks?
-  MonogatariObjectManager.prototype.getGlobalNode = function( id ) {
-    if ( this._objects.contains( id ) ) {
-      if ( this._objects.get( id ).parent === 'trunk' ) {
-        return this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).scale;
-      } else {
-        var parentNode = this.getGlobalNode( this._objects.get( id ).parent ),
-            node = new _Node( this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).position.clone(),
-                                        this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).rotation.clone(),
-                                        this._objects.get( id ).value.findComponent( _Base.COMPONENT_NODE ).scale.clone() );
-
-        node.position.add( parentNode.position.getX(), parentNode.position.getY(), parentNode.position.getZ() );
-        node.rotation.add( parentNode.rotation.getX(), parentNode.rotation.getY(), parentNode.rotation.getZ() );
-        node.scale.add( parentNode.scale.getX(), parentNode.scale.getY(), parentNode.scale.getZ() );
-
-        return node;
-      }
-    }
-    return null;
-  };
-
-  Monogatari.ObjectManager = new MonogatariObjectManager();
+  ObjectManager.prototype.getGlobalPosition = function() {};
+  ObjectManager.prototype.getGlobalRotation = function() {};
+  ObjectManager.prototype.getGlobalScale = function() {};
+  ObjectManager.prototype.getGlobalNode = function() {};
 
 } );
