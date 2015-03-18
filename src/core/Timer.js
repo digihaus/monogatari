0001ef88083314ebf9c7af620b1b6bbd1737af51
Marcio Lucca <mrclucca@gmail.com>
2015-03-17 22:40:02 -0300

    Removing cycle time

--- src/core/Timer.js
+++ src/core/Timer.js
@@ -7,7 +7,6 @@ define( function() {
     this.frameTicks = 0;
     this.fps = 60;
 
-    this.CYCLE_TIME = 1e16;
     this.FRAME_RATE_60FPS = 0.016666666667; // 1.0 second / 60.0 frames
   };
 
@@ -17,11 +16,6 @@ define( function() {
     // Use Date.now() instead of new Date().getTime(), avoids one object allocation
     var now = Date.now();
     var delta = now - this.lastTime;
-
-    // Used to not overflow time value
-    if ( this.time > this.CYCLE_TIME ) {
-      this.time = 0;
-    }
 
     this.time += delta;
     this.lastTime = now;
@@ -42,11 +36,6 @@ define( function() {
 
     // Stores frame ticks per cycle
     this.frameTicks++;
-  };
-
-  // Returns the difference in milliseconds from the given time, to current Monogatari time
-  Timer.prototype.compare = function( time ) {
-    return ( time > this.time ) ? this.time + this.CYCLE_TIME - time : this.time - time;
   };
 
   return Timer;
