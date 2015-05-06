0595f945c64250fd223b71814b013ece2c6a338d
marciolucca <mrclucca@gmail.com>
2015-05-06 16:42:15 -0300

    Testing modularization of demo source

---
+++ demo/endless zombies/hero.js
@@ -0,0 +1,41 @@
+define( [ 'Monogatari', '../endless zombies/Bullets' ], function( m, Bullets ) {
+
+  var hero = new m.GameObject( 'hero' );
+
+  // center the hero on the screen
+  hero.position.set( m.sceneManager.canvasWidth / 2, m.sceneManager.canvasHeight / 2, 0 );
+
+  var sprite = new m.Sprite( 'assets/sprites/generico_tosco.png', 64, 64 );
+  hero.addComponent( sprite );
+  hero.life = 100;
+
+  hero.update = function() {
+    var speed = 200 / m.timer.fps;
+
+    if ( m.keyboard.isDown( m.keyboard.W ) )
+      this.position.y -= speed;
+    if ( m.keyboard.isDown( m.keyboard.A ) )
+      this.position.x -= speed;
+    if ( m.keyboard.isDown( m.keyboard.S ) )
+      this.position.y += speed;
+    if ( m.keyboard.isDown( m.keyboard.D ) )
+      this.position.x += speed;
+
+    if ( m.keyboard.isDown( m.keyboard.UP_ARROW ) )
+      this.position.y -= speed;
+    if ( m.keyboard.isDown( m.keyboard.LEFT_ARROW ) )
+      this.position.x -= speed;
+    if ( m.keyboard.isDown( m.keyboard.DOWN_ARROW ) )
+      this.position.y += speed;
+    if ( m.keyboard.isDown( m.keyboard.RIGHT_ARROW ) )
+      this.position.x += speed;
+
+    if ( m.mouse.isDown( m.mouse.LMB ) )
+      Bullets.shoot( this.position.x, this.position.y );
+
+    this.lookAt( m.mouse.position );
+  };
+
+  return hero;
+
+} );
