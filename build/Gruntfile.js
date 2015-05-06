1eb316fb9501eeaf77e3fc19c30740c544338dc6
marciolucca <mrclucca@gmail.com>
2015-05-06 16:40:45 -0300

    Removing live reload

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,55 +1,54 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
+module.exports = function( grunt ) {
+  grunt.initConfig( {
 
-    pkg: grunt.file.readJSON('package.json'),
+    pkg : grunt.file.readJSON( 'package.json' ),
 
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: '../src/',
-          paths: {
-            requireLib: '../lib/Require',
-            lib: '../lib'
+    requirejs : {
+      compile : {
+        options : {
+          baseUrl : '../src/',
+          paths : {
+            requireLib : '../lib/Require',
+            lib : '../lib'
           },
-          name: 'Monogatari',
-          include: ['requireLib'],
-          out: '../dist/monogatari-<%= pkg.version %>.js'
+          name : 'Monogatari',
+          include : [ 'requireLib' ],
+          out : '../dist/monogatari-<%= pkg.version %>.js'
         }
       }
     },
 
-    uglify: {
-      dist: {
-        src: ['../dist/monogatari-<%= pkg.version %>.js'],
-        dest: '../dist/monogatari-<%= pkg.version %>.min.js',
+    uglify : {
+      dist : {
+        src : [ '../dist/monogatari-<%= pkg.version %>.js' ],
+        dest : '../dist/monogatari-<%= pkg.version %>.min.js',
       }
     },
 
-    watch: {
-      all: {
-        files: '../src/**/*',
-        tasks: ['requirejs', 'uglify']
+    watch : {
+      all : {
+        files : '../src/**/*',
+        tasks : [ 'requirejs', 'uglify' ]
       }
     },
 
-    connect: {
-      server: {
-        options: {
-          port: 9000,
-          livereload: true,
-          base: "../"
+    connect : {
+      server : {
+        options : {
+          port : 9000,
+          base : "../"
         }
       }
     }
 
-  });
+  } );
 
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-connect' );
 
-  grunt.registerTask('default', ['requirejs', 'uglify']);
+  grunt.registerTask( 'default', [ 'requirejs', 'uglify' ] );
 
-  grunt.registerTask('live', ['connect', 'watch']);
+  grunt.registerTask( 'live', [ 'connect', 'watch' ] );
 };
