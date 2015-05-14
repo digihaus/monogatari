56daf1113d45525528be2ee6d43278e0f5a0ade1
marciolucca <mrclucca@gmail.com>
2015-05-14 15:26:43 -0300

    Formatting

--- src/render/Camera2D.js
+++ src/render/Camera2D.js
@@ -1,22 +1,25 @@
 /**
- * This is an utilitary class to hold the THREE camera itself and the information of which scenes should be rendered in which camera
+ * Utility class that holds the THREE camera and the information of which scenes should be rendered in which camera.
  */
-define( [ 'collection/Map', 'lib/Three' ], function( _Map, _Three ) {
+define(
+  [ 'lib/Three' ], function ( Three ) {
 
-  var Camera2D = function( left, right, top, bottom, near, far ) {
-    // a THREE.Camera object
-    this.cam = new THREE.OrthographicCamera( left, right, top, bottom, near, far );
-    // translate the point of origin (0,0,0), to the top left corner
-    this.cam.position.set( right, bottom, far );
+    var Camera2D = function ( left, right, top, bottom, near, far ) {
+      // A THREE.Camera object
+      this.cam = new THREE.OrthographicCamera( left, right, top, bottom, near, far );
 
-    this.scenes = [];
-  };
+      // Translates the point of origin (0,0,0), to the top left corner
+      this.cam.position.set( right, bottom, far );
 
-  Camera2D.prototype.addScene = function( sceneId, scene ) {
-    if ( sceneId && typeof sceneId === 'string' ) {
-      this.scenes.push( sceneId );
-    }
+      this.scenes = [];
+    };
+
+    Camera2D.prototype.addScene = function ( sceneId, scene ) {
+      if ( sceneId && typeof sceneId === 'string' ) {
+        this.scenes.push( sceneId );
+      }
+    };
+
+    return Camera2D;
   }
-
-  return Camera2D;
-} );
+);
