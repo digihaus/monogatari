04d194d2c83506c00c00ad23d33d3f88b3ceff18
Chrysthian <suisen.no.ryuu@gmail.com>
2015-07-09 21:08:04 -0300

    formatting, implementing listener functions

--- src/main/manager/PhysicsManager.js
+++ src/main/manager/PhysicsManager.js
@@ -1,82 +1,82 @@
-define( [ 'lib/Box2d', 'core/Timer' ], function( _Box2d, Timer ) {
+define(
+  [ 'lib/Box2d', 'core/Timer' ], function( _Box2d, Timer ) {
 
-  var PhysicsManager = function() {
-    this.world = null;
+    var PhysicsManager = function() {
+      this.world = null;
 
-    // The more iterations, the more accurate the calculations
-    this.velocityIterations = 2;
-    this.positionIterations = 2;
+      // The more iterations, the more accurate the calculations
+      this.velocityIterations = 2;
+      this.positionIterations = 2;
 
-    // Remove any accumulated forces at every physics update
-    this.clearForcesOnUpdate = false;
-  };
-
-  PhysicsManager.prototype.createWorld = function( gravity, allowSleep ) {
-    this.world = new Box2D.b2World( new Box2D.b2Vec2( gravity.x, gravity.y ), allowSleep );
-    this.createListener();
-  };
-
-  PhysicsManager.prototype.attachToWorld = function( bodyDef ) {
-    return this.world.CreateBody( bodyDef );
-  };
-
-  PhysicsManager.prototype.destroyBody = function( bodyDef ) {
-    return this.world.DestroyBody( bodyDef );
-  };
-
-  PhysicsManager.prototype.createListener = function() {
-    var listener = new Box2D.JSContactListener();
-
-    listener.BeginContact = function ( _contact ) {
-      var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-      //console.log( contact.GetFixtureA() );
-    }
-
-    listener.EndContact = function( _contact ) {
-      var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-      //console.log( contact.GetFixtureA() );
+      // Remove any accumulated forces at every physics update
+      this.clearForcesOnUpdate = false;
     };
 
-/*
-    listener.PreSolve = function( _contact, _oldManifold ) {
-      var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-      var manifold = Box2D.wrapPointer( _oldManifold, Box2D.b2Manifold );
-      //console.log( contact.GetFixtureA() );
+    PhysicsManager.prototype.createWorld = function( gravity, allowSleep ) {
+      this.world = new Box2D.b2World( new Box2D.b2Vec2( gravity.x, gravity.y ), allowSleep );
+      this.createListener();
     };
 
-    listener.PostSolve = function( _contact, _impulse ) {
-      var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-      var impulse = Box2D.wrapPointer( _contact, Box2D.b2ContactImpulse );
-      //console.log( contact.GetFixtureA() );
+    PhysicsManager.prototype.attachToWorld = function( bodyDef ) {
+      return this.world.CreateBody( bodyDef );
     };
-*/
 
-    this.world.SetContactListener( listener );
-  };
+    PhysicsManager.prototype.destroyBody = function( bodyDef ) {
+      return this.world.DestroyBody( bodyDef );
+    };
 
-  PhysicsManager.prototype.update = function() {
-    if ( this.world ) {
-      var fps = Timer.fps;
+    PhysicsManager.prototype.createListener = function() {
+      var listener = new Box2D.JSContactListener();
 
-      // Frame rate at which to update physics ( 1 / FPS or 1.0 / 60.0 )
-      var physicsFrameRate = ( fps ) ? 1 / fps : Timer.FRAME_RATE_60FPS;
+      listener.BeginContact = function( _contact ) {
+        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+        // console.log( "BeginContact" + contact.GetFixtureA() );
+      };
 
-      this.world.Step( physicsFrameRate, this.velocityIterations, this.positionIterations );
+      listener.EndContact = function( _contact ) {
+        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+        // console.log( "EndContact" + contact.GetFixtureA() );
+      };
 
-      if ( this.clearForcesOnUpdate ) {
-        this.world.ClearForces();
+      listener.PreSolve = function( _contact, _oldManifold ) {
+        //        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+        //        var manifold = Box2D.wrapPointer( _oldManifold, Box2D.b2Manifold );
+        //        console.log( "PreSolve" + contact.GetFixtureA() );
+      };
+
+      listener.PostSolve = function( _contact, _impulse ) {
+        //        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+        //        var impulse = Box2D.wrapPointer( _contact, Box2D.b2ContactImpulse );
+        //        console.log( "PostSolve" + contact.GetFixtureA() );
+      };
+
+      this.world.SetContactListener( listener );
+    };
+
+    PhysicsManager.prototype.update = function() {
+      if( this.world ) {
+        var fps = Timer.fps;
+
+        // Frame rate at which to update physics ( 1 / FPS or 1.0 / 60.0 )
+        var physicsFrameRate = ( fps ) ? 1 / fps : Timer.FRAME_RATE_60FPS;
+
+        this.world.Step( physicsFrameRate, this.velocityIterations, this.positionIterations );
+
+        if( this.clearForcesOnUpdate ) {
+          this.world.ClearForces();
+        }
       }
-    }
-  };
+    };
 
-  var instance = null;
+    var instance = null;
 
-  PhysicsManager.getInstance = function() {
-    if ( instance === null ) {
-      instance = new PhysicsManager();
-    }
-    return instance;
-  };
+    PhysicsManager.getInstance = function() {
+      if( instance === null ) {
+        instance = new PhysicsManager();
+      }
+      return instance;
+    };
 
-  return PhysicsManager.getInstance();
-} );
+    return PhysicsManager.getInstance();
+  }
+);
