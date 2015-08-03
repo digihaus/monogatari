7508dd0b98d4ee97960af20f6434ab0e57ddd6ca
chrysthian <chrysthian.simao@gmail.com>
2015-08-03 14:53:51 -0300

    Improved PhysicsManager collision events

--- src/main/manager/PhysicsManager.js
+++ src/main/manager/PhysicsManager.js
@@ -10,10 +10,19 @@ define(
 
       // Remove any accumulated forces at every physics update
       this.clearForcesOnUpdate = false;
+
+      this.BEGIN_CONTACT = 1; // 0001
+      this.END_CONTACT = 2; // 0010
+      this.PRE_SOLVE = 4; // 0100
+      this.POST_SOLVE = 8; // 1000
+      this.ALL_LISTENERS = 15; //1111
+
+      this.listeners = 0; //0000
     };
 
-    PhysicsManager.prototype.createWorld = function( gravity, allowSleep ) {
-      this.world = new Box2D.b2World( new Box2D.b2Vec2( gravity.x, gravity.y ), allowSleep );
+    PhysicsManager.prototype.createWorld = function( gravity, allowSleep, listeners ) {
+      this.world = new Box2D.b2World( new Box2D.b2Vec2( gravity.x, gravity.y ), allowSleep || false );
+      this.listeners = ( listeners ) ? listeners : this.BEGIN_CONTACT | this.END_CONTACT; //0011
       this.createListener();
     };
 
@@ -28,53 +37,64 @@ define(
     PhysicsManager.prototype.createListener = function() {
       var listener = new Box2D.JSContactListener();
 
-      listener.BeginContact = function( _contact ) {
-        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-        var aUid = contact.GetFixtureA().GetUserData();
-        var bUid = contact.GetFixtureB().GetUserData();
+      listener.BeginContact = ( this.listeners & this.BEGIN_CONTACT ) ?
+        function( _contact ) {
+          var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+          var aUid = contact.GetFixtureA().GetUserData();
+          var bUid = contact.GetFixtureB().GetUserData();
 
-        if( aUid && bUid ) {
-          MessageManager.register( new Message( aUid, bUid, "BeginContact", contact ) );
-          MessageManager.register( new Message( bUid, aUid, "BeginContact", contact ) );
-        }
+          if( aUid ) {
+            MessageManager.registerEngineMessage( new Message( aUid, bUid, "BeginContact", contact ) );
+          }
+          if( bUid ) {
+            MessageManager.registerEngineMessage( new Message( bUid, aUid, "BeginContact", contact ) );
+          }
 
-      };
+        } : function() {};
 
-      listener.EndContact = function( _contact ) {
-        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-        var aUid = contact.GetFixtureA().GetUserData();
-        var bUid = contact.GetFixtureB().GetUserData();
+      listener.EndContact = ( this.listeners & this.END_CONTACT ) ?
+        function( _contact ) {
+          var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+          var aUid = contact.GetFixtureA().GetUserData();
+          var bUid = contact.GetFixtureB().GetUserData();
 
-        if( aUid && bUid ) {
-          MessageManager.register( new Message( aUid, bUid, "EndContact", contact ) );
-          MessageManager.register( new Message( bUid, aUid, "EndContact", contact ) );
-        }
-      };
+          if( aUid ) {
+            MessageManager.registerEngineMessage( new Message( aUid, bUid, "EndContact", contact ) );
+          }
+          if( bUid ) {
+            MessageManager.registerEngineMessage( new Message( bUid, aUid, "EndContact", contact ) );
+          }
+        } : function() {};
 
-      listener.PreSolve = function( _contact, _oldManifold ) {
-        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-        var manifold = Box2D.wrapPointer( _oldManifold, Box2D.b2Manifold );
-        var aUid = contact.GetFixtureA().GetUserData();
-        var bUid = contact.GetFixtureB().GetUserData();
+      listener.PreSolve = ( this.listeners & this.PRE_SOLVE ) ?
+        function( _contact, _oldManifold ) {
+          var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+          var manifold = Box2D.wrapPointer( _oldManifold, Box2D.b2Manifold );
+          var aUid = contact.GetFixtureA().GetUserData();
+          var bUid = contact.GetFixtureB().GetUserData();
 
-        if( aUid && bUid ) {
-          MessageManager.register( new Message( aUid, bUid, "PreSolve", { contact: contact, manifold: manifold } ) );
-          MessageManager.register( new Message( bUid, aUid, "PreSolve", { contact: contact, manifold: manifold } ) );
-        }
+          if( aUid ) {
+            MessageManager.registerEngineMessage( new Message( aUid, bUid, "PreSolve", { contact: contact, manifold: manifold } ) );
+          }
+          if( bUid ) {
+            MessageManager.registerEngineMessage( new Message( bUid, aUid, "PreSolve", { contact: contact, manifold: manifold } ) );
+          }
+        } : function() {};
 
-      };
+      listener.PostSolve = ( this.listeners & this.POST_SOLVE ) ?
+        function( _contact, _impulse ) {
+          var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
+          var impulse = Box2D.wrapPointer( _impulse, Box2D.b2ContactImpulse );
+          var aUid = contact.GetFixtureA().GetUserData();
+          var bUid = contact.GetFixtureB().GetUserData();
 
-      listener.PostSolve = function( _contact, _impulse ) {
-        var contact = Box2D.wrapPointer( _contact, Box2D.b2Contact );
-        var impulse = Box2D.wrapPointer( _impulse, Box2D.b2ContactImpulse );
-        var aUid = contact.GetFixtureA().GetUserData();
-        var bUid = contact.GetFixtureB().GetUserData();
-
-        if( aUid && bUid ) {
-          MessageManager.registerEngineMessage( new Message( aUid, bUid, "PostSolve", { contact: contact, impulse: impulse } ) );
-          MessageManager.registerEngineMessage( new Message( bUid, aUid, "PostSolve", { contact: contact, impulse: impulse } ) );
-        }
-      };
+          if( aUid ) {
+            MessageManager.registerEngineMessage( new Message( aUid, bUid, "PostSolve", { contact: contact, impulse: impulse } ) );
+          }
+          if( bUid ) {
+            MessageManager.registerEngineMessage( new Message( bUid, aUid, "PostSolve", { contact: contact, impulse: impulse } ) );
+          }
+        } : function() {};
 
       this.world.SetContactListener( listener );
     };
