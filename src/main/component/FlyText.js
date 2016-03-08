108ed30afe0957d9e9f11802f26b1027ba6189b0
chrysthian <chrysthian.simao@gmail.com>
2016-03-08 16:38:04 -0300

    Javadoc

--- src/main/component/FlyText.js
+++ src/main/component/FlyText.js
@@ -1,20 +1,94 @@
+/**
+ * Exports the {@link module:component/FlyText~FlyText|FlyText} class.
+ * @module component/FlyText
+ */
 define(
   [ 'core/Timer', 'component/Base', 'component/Text', 'lib/Three', 'render/Context2D' ], function( Timer, Base, Text, _Three, Context2D ) {
 
+    /**
+     * Moving Text display. It creates an invisible "bounding box" around the Text to limit and break lines using the parameters width and height,
+     * displaying characters on by one, by the rate of the speed parameter.
+     *
+     * @param {String} [text] String containing the text to be displayed
+     * @param {String} [fontSize] Font size in pixels
+     * @param {String} [fontFamily]  Font family name
+     * @param {Number} [width] Width in pixels for the area containing the text.
+     * @param {Number} [height] Height in pixels for the area containing the text.
+     * @param {String} [color] Hexadecimal color
+     * @param {Number} [speed] Interval in milliseconds between each character to be displayed.
+     * @class FlyText
+     */
     var FlyText = function( text, fontSize, fontFamily, width, height, color, speed ) {
       Text.call( this, text, fontSize, fontFamily, width, height, color );
+
+      /**
+       * Component Type.
+       * @memberOf module:component/FlyText~FlyText
+       * @instance
+       * @type {Number}
+       * @name type
+       */
       this.type = Base.FLY_TEXT;
 
+      /**
+       * Interval in milliseconds between each character to be displayed.
+       * @memberOf module:component/FlyText~FlyText
+       * @instance
+       * @type {Number}
+       * @default 30
+       * @name speed
+       */
       this.speed = ( speed ) ? speed : 30;
+
+      /**
+       * Buffer used to display part of the text.
+       * @memberOf module:component/FlyText~FlyText
+       * @instance
+       * @type {String}
+       * @default ''
+       * @name textBuffer
+       */
       this.textBuffer = '';
+
+      /**
+       * Time that the component started displaying the text.
+       * @memberOf module:component/FlyText~FlyText
+       * @instance
+       * @type {Number}
+       * @default 0
+       * @name startTime
+       */
       this.startTime = 0;
 
       this.load();
+
+      /**
+       * Flag to point if the component has finished drawing all characters.
+       * @memberOf module:component/FlyText~FlyText
+       * @instance
+       * @type {Boolean}
+       * @default false
+       * @name finished
+       */
       this.finished = false;
     };
 
     FlyText.prototype = Object.create( Text.prototype );
 
+    /**
+     * Sets various properties of the font then re-parses for proper buffering.
+     * @method
+     * @instance
+     * @name reset
+     * @param {String} [text] Text to be displayed
+     * @param {Number} [fontSize] Font size, in pixels
+     * @param {String} [fontFamily] Font-Family, accepts web fonts
+     * @param {Number} [width] Buffer width, in pixels
+     * @param {Number} [height] Buffer height, in pixels
+     * @param {String} [color] Hexadecimal color
+     * @param {Number} [speed] Interval in milliseconds between each character to be displayed.
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.reset = function( text, fontSize, fontFamily, width, height, color, speed ) {
       this.text = ( text ) ? text : 'The quick brown fox jumps over the lazy dog';
       this.fontSize = ( fontSize ) ? fontSize : 10;
@@ -30,6 +104,14 @@ define(
       this.finished = false;
     };
 
+
+    /**
+     * Clears the internal buffer for repaint.
+     * @method
+     * @instance
+     * @name clearBuffer
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.clearBuffer = function() {
       var context = this.buffer.getContext( '2d' );
       context.clearRect( 0, 0, this.w, this.h );
@@ -38,21 +120,51 @@ define(
       this.startTime = 0;
     };
 
+    /**
+     * Set the Text then re-parses for proper buffering.
+     * @method
+     * @instance
+     * @name setText
+     * @param {String} [text] Text to be displayed
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.setText = function( text ) {
       this.text = ( text ) ? text : '';
       this.clearBuffer();
     };
 
+    /**
+     * Set the time in milliseconds then re-parses for proper buffering.
+     * @method
+     * @instance
+     * @name setText
+     * @param {Number} [speed] Interval in milliseconds between each character to be displayed.
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.setSpeed = function( speed ) {
       this.speed = ( speed ) ? speed : 30;
       this.clearBuffer();
     };
 
+    /**
+     * Callback function, triggered by the engine
+     * @method
+     * @instance
+     * @name onLoad
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.onLoad = function() {
       this.parse();
       this.createTexture();
     };
 
+    /**
+     * Create the texture buffer based on canvas, and attach it to the component material and mesh for rendering.
+     * @method
+     * @instance
+     * @name createTexture
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.createTexture = function() {
       this.texture = ( this.texture ) ? this.texture : new THREE.Texture();
       this.texture.needsUpdate = true;
@@ -69,6 +181,14 @@ define(
       this.isLoaded = true;
     };
 
+    /**
+     * Heartbeat to update the characters displayed based on time.<br>
+     * When finished, flags it on the component.
+     * @method
+     * @instance
+     * @name update
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.update = function() {
 
       if( this.startTime === 0 ) {
@@ -99,6 +219,14 @@ define(
       }
     };
 
+    /**
+     * Renders the text with properties defined into the internal buffer texture, NOT on the screen.
+     * It is rendered on screen only during the SceneManager render cycle.
+     * @method
+     * @instance
+     * @name renderIntoBuffer
+     * @memberOf module:component/FlyText~FlyText
+     */
     FlyText.prototype.renderIntoBuffer = function() {
       var context = this.buffer.getContext( '2d' );
       context.imageSmoothingEnabled = false;
