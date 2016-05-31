59395eb7f3a8fa4628aa017a57458afa79840b62
marciolucca <mrclucca@gmail.com>
2016-05-31 19:07:25 -0300

    General cleansing for release

--- build.js
+++ build.js
@@ -1,15 +1,15 @@
 ({
-    name: 'Monogatari',
-    baseUrl: './src',
-    paths: {
-        'lib': '../lib',
-        'lib/Three': '../lib/three.min',
-        'lib/Box2d': '../lib/box2d',
-        'lib/Howler': '../lib/howler'
-    },
-    preserveLicenseComments: false,
-    out: './build/' + process.env.npm_package_name + '.min.js',
-    wrap: {
-        start: '// ' + process.env.npm_package_name + '-r' + process.env.npm_package_revision
-    }
+  name: 'Monogatari',
+  baseUrl: './src',
+  paths: {
+    'lib': '../lib',
+    'lib/Three': '../lib/three.min',
+    'lib/Box2d': '../lib/box2d',
+    'lib/Howler': '../lib/howler'
+  },
+  preserveLicenseComments: false,
+  out: './build/' + process.env.npm_package_name + '.min.js',
+  wrap: {
+    start: '// ' + process.env.npm_package_name + '-r' + process.env.npm_package_revision
+  }
 })
--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,7 @@ require.config(
             'lib': '../lib',
             'lib/Three': '../lib/three.min',
             'lib/Box2d': '../lib/box2d',
-            'lib/Howler': '../lib/howler',
+            'lib/Howler': '../lib/howler'
         }
     }
 );
