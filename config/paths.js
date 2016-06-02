600cf60d530ebf2564f7c3aa35ce0ea4b9f62c5b
marciolucca <mrclucca@gmail.com>
2016-06-02 13:07:28 -0300

    Renaming box2d lib file

--- config/paths.js
+++ config/paths.js
@@ -2,7 +2,7 @@ define(function() {
   return paths = {
     'lib': '../lib',
     'lib/Three': '../lib/three.min',
-    'lib/Box2d': '../lib/box2d',
+    'lib/Box2d': '../lib/box2d.min',
     'lib/Howler': '../lib/howler.min'
   };
 });
