5ce376d7873fee91bc2cd144dc42b93003b99cf7
marciolucca <mrclucca@gmail.com>
2016-06-05 16:10:56 -0300

    Docs and docs layout

--- src/collection/Tree.js
+++ src/collection/Tree.js
@@ -1,38 +1,27 @@
-/**
- * Exports the {@link module:collection/Tree~Tree|Tree} class.
- * @module collection/Tree
- */
 define(
   [ 'core/Common' ], function( Common ) {
 
     /**
-     * A specific Node used for the collection Tree
-     *
+     * A specific node used for the {@link module:collection/Tree|collection/Tree}.
      * @class TreeNode
      */
     var TreeNode = function( data, parent ) {
       /**
-       * Actual data stored in the node
-       * @memberOf module:collection/Tree~TreeNode
-       * @instance
+       * Actual data stored in the node.
+       * @memberOf TreeNode
        * @type {Object}
-       * @name data
        */
       this.data = data;
       /**
-       * Pointer to the parent Node
-       * @memberOf module:collection/Tree~TreeNode
-       * @instance
+       * Pointer to the parent node.
+       * @memberOf TreeNode
        * @type {TreeNode}
-       * @name parent
        */
       this.parent = parent;
       /**
-       * Array of pointers of children Nodes
-       * @memberOf module:collection/Tree~TreeNode
-       * @instance
+       * Array of pointers of children nodes.
+       * @memberOf TreeNode
        * @type {Array}
-       * @name children
        */
       this.children = [];
     };
@@ -41,22 +30,18 @@ define(
      * Collection of data elements sorted hierarchically from root to leaf, with child nodes connected by means of a pointer.
      * Elements can easily be inserted or removed without reallocation or reorganization of the entire structure
      * because the data items need not be stored contiguously in memory.
-     *
-     * @class Tree
+     * @requires core/Common
+     * @exports collection/Tree
      */
     var Tree = function() {
       this.root = new TreeNode( 'root' );
     };
 
     /**
-     * Find a given value in the Tree and returns the TreeNode containing it or null if not found.
-     * @method
-     * @instance
-     * @name find
-     * @param {Object} data The value to be searched
-     * @param {TreeNode} [startNode] A pointer to a Tree node, start from root if not provided
+     * Finds a given value in the tree and returns the TreeNode containing it or null if not found.
+     * @param {Object} data - The value to be searched
+     * @param {TreeNode} [startNode] - A pointer to a TreeNode, start from root if not provided
      * @return {TreeNode}
-     * @memberOf module:collection/Tree~Tree
      */
     Tree.prototype.find = function( data, startNode ) {
       var sNode = startNode ? startNode : this.root;
@@ -79,13 +64,9 @@ define(
     };
 
     /**
-     * Put a value inside the tree under the given parent node;
-     * @method
-     * @instance
-     * @name put
-     * @param {Object} data Description
-     * @param {TreeNode} [parent] Description
-     * @memberOf module:collection/Tree~Tree
+     * Puts a value inside the tree under the given parent node.
+     * @param {Object} data
+     * @param {TreeNode} [parent]
      */
     Tree.prototype.put = function( data, parent ) {
       var pNode;
@@ -98,11 +79,7 @@ define(
 
     /**
      * Remove a node based on given value.
-     * @method
-     * @instance
-     * @name remove
-     * @param {Object} data Value stored on a TreeNode.
-     * @memberOf module:collection/Tree~Tree
+     * @param {Object} data - Value stored on a TreeNode.
      */
     Tree.prototype.remove = function( data ) {
       var node = this.find( data );
@@ -122,24 +99,16 @@ define(
     };
 
     /**
-     * Clear entire Tree and flags it to garbage collection.
-     * @method
-     * @instance
-     * @name clear
-     * @memberOf module:collection/Tree~Tree
+     * Clears the entire tree and flags it for garbage collection.
      */
     Tree.prototype.clear = function() {
       this.root.children.length = 0;
     };
 
     /**
-     * Move a given data to the new given parent
-     * @method
-     * @instance
-     * @name move
-     * @param {Object} data Value
-     * @param {TreeNode} parent Parent node
-     * @memberOf module:collection/Tree~Tree
+     * Move a given data to the new given parent.
+     * @param {Object} data - Value
+     * @param {TreeNode} parent - Parent node
      */
     Tree.prototype.move = function( data, parent ) {
       remove( data );
@@ -148,10 +117,6 @@ define(
 
     /**
      * List of immediate children of the given data.
-     * @method
-     * @instance
-     * @name listChildren
-     * @memberOf module:collection/Tree~Tree
      * @return {Array} Array of child nodes
      */
     Tree.prototype.listChildren = function( data ) {
@@ -165,10 +130,6 @@ define(
 
     /**
      * List of all children of the given data.
-     * @method
-     * @instance
-     * @name listDescendants
-     * @memberOf module:collection/Tree~Tree
      * @return {Array} Array of child nodes
      */
     Tree.prototype.listDescendants = function( data ) {
@@ -190,11 +151,7 @@ define(
     };
 
     /**
-     * Transforms the Tree values on an array
-     * @method
-     * @instance
-     * @name toArray
-     * @memberOf module:collection/Tree~Tree
+     * Transforms the tree values to an Array.
      * @return {Array}
      */
     Tree.prototype.toArray = function() {
