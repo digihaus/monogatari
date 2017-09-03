eb284cc8ad51ea8c82d4aebcc7c611da30e14c38
marciolucca <mrclucca@gmail.com>
2017-09-03 19:12:14 +0200

    Configuring CI

--- config/deploy.js
+++ config/deploy.js
@@ -6,16 +6,15 @@ const deploy = () => {
     console.log('Running deploy...'.grey);
 
     var version = process.env.npm_package_version.split('-')[0];
-    var repo = 'https://' + process.env.GH_TOKEN + '@github.com/digihaus/monogatari.git';
 
     fs.copySync('dist/latest', 'dist/' + version);
 
-    ghpages.publish('dist/latest', { dest: 'latest', repo: repo}, function (err) {
+    ghpages.publish('dist/latest', { dest: 'latest' }, function (err) {
         if (err) {
             console.log('Error deploying to gh-pages'.red);
         } else {
             console.log('Done deploy to gh-pages '.grey + 'latest/'.cyan);
-            ghpages.publish('dist/' + version, { dest: version, repo: repo}, function (err) {
+            ghpages.publish('dist/' + version, { dest: version }, function (err) {
                 if (err) {
                     console.log('Error deploying to gh-pages'.red);
                 } else {
