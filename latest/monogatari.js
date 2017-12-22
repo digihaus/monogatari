0d8e9c9c45883eb7abdbd2ba9291ae61909f6741
Travis CI User <travis@example.org>
2017-12-22 11:27:13 +0000

    Updates

--- latest/monogatari.js
+++ latest/monogatari.js
@@ -50154,10 +50154,8 @@ var Box2D = require('link/Box2D');
  * Box2D has been tuned to work well with moving objects between 0.1 and 10 meters. 
  * This means objects between soup cans and buses in size should work well. 
  * Static objects may be up to 50 meters big without too much trouble.
- * Box2D is tuned for meters, kilograms, and seconds.
+ * Considered units are meters, kilograms and seconds.
  *
- * @param {RigidBody.TYPE} type - Type of this Rigid Body: static, kinematic or dynamic
- * @param {b2Shape} shape - Geometrical shape of this Rigid Body
  * @extends component/Base
  * @exports component/RigidBody
  */
@@ -50171,28 +50169,42 @@ var RigidBody = function (_Base) {
 
     /**
      * Enumeration of rigid body types.
-     * @type {Number}
-     * @enum
+     * @enum {number}
+     * @property {number} STATIC A body which isn’t affected by world forces, it does not react to collisions. It can’t be moved.
+     * @property {number} KINEMATIC An hybrid body which is not affected by forces and collisions like a static body but can be moved with a linear velocity like a dynamic body.
+     * @property {number} DYNAMIC A body which is affected by world forces and reacts to collisions.
      */
     get: function get() {
       return {
-        /** A <b>static body</b> is a body which isn’t affected by world forces it does not react to collisions. It can’t be moved. */
         STATIC: Box2D.b2_staticBody,
-        /** A <b>kinematic body</b> is an hybrid body which is not affected by forces and collisions like a static body but can moved with a linear velocity like a dynamic body. */
         KINEMATIC: Box2D.b2_kinematicBody,
-        /** A <b>dynamic body</b> is a body which is affected by world forces and react to collisions. */
         DYNAMIC: Box2D.b2_dynamicBody
       };
     }
   }]);
 
+  /**
+   * Creates a RigidBody.
+   * @param {RigidBody.TYPE} type - Type of the body: static, kinematic or dynamic
+   * @param {b2Shape} shape - Geometrical shape of the body
+   * @param {Object} [options]
+   * @param {number} [options.density] - Material density in kg/m^2
+   * @param {number} [options.friction] - Material friction, usually in the range [0,1]
+   * @param {number} [options.bounciness] - Material bounciness, usually in the range [0,1]
+   * @param {number} [options.angle] - Body rotation angle in radians
+   * @param {boolean} [options.isSensor] - Prevents the collision to be resolved by Box2D, but retains collision information
+   * @param {boolean} [options.preventTunneling] - Prevents the collision to be resolved through other objects. <b>Expensive! Use with care.</b>
+   * @param {boolean} [options.allowRotation] - Prevents or allows the rotation on the body
+   */
   function RigidBody(type, shape) {
+    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
     _classCallCheck(this, RigidBody);
 
     var _this = _possibleConstructorReturn(this, (RigidBody.__proto__ || Object.getPrototypeOf(RigidBody)).call(this, Base.TYPE.RIGID_BODY));
 
-    if (!Object.values(RigidBody.TYPE).includes(type)) throw new Error('Param "type" is invalid.');
-    if (shape === undefined) throw new Error('Param shape is required.');
+    if (!Object.values(RigidBody.TYPE).includes(type)) throw new Error('Illegal argument "type".');
+    if (shape === undefined) throw new Error('Illegal argument "shape".');
 
     /**
      * The physics body definition (Box2D.BodyDef) from this component.
@@ -50216,42 +50228,29 @@ var RigidBody = function (_Base) {
     _this.body = null;
 
     _this.bodyDef.set_type(type);
+    if (options.preventTunneling) _this.bodyDef.set_bullet(options.preventTunneling);
+    if (options.allowRotation) _this.bodyDef.set_fixedRotation(!options.allowRotation);
+    if (options.angle) _this.bodyDef.angle.set_angle(options.angle);
+
     _this.materialDef.set_shape(shape);
+    if (options.density) _this.materialDef.set_density(options.density);
+    if (options.friction) _this.materialDef.set_friction(options.friction);
+    if (options.bounciness) _this.materialDef.set_restitution(options.bounciness);
+    if (options.isSensor) _this.materialDef.set_isSensor(options.isSensor);
     return _this;
   }
 
   /**
-   * Sets the density of the material.
-   * @param {number} density - Density in kg/m^2
+   * Allows to store a data (in the means of a pointer) of an object to work with the internal memory of the Box2D.
+   * It is (kinda) bugged on emscripten port, but can be {@link https://github.com/kripken/box2d.js/issues/35|worked around}.
+   * @param {Object} userData
    */
 
 
   _createClass(RigidBody, [{
-    key: 'setDensity',
-    value: function setDensity(density) {
-      this.materialDef.set_density(density);
-    }
-
-    /**
-     * Sets the friction of the material.
-     * @param {number} friction - Usually in the range [0,1]
-     */
-
-  }, {
-    key: 'setFriction',
-    value: function setFriction(friction) {
-      this.materialDef.set_friction(friction);
-    }
-
-    /**
-     * Sets the bounciness of the material
-     * @param {number} bounciness - Usually in the range [0,1]
-     */
-
-  }, {
-    key: 'setBounciness',
-    value: function setBounciness(bounciness) {
-      this.materialDef.set_restitution(bounciness);
+    key: 'setUserData',
+    value: function setUserData(userData) {
+      this.materialDef.set_userData(userData);
     }
 
     /**
@@ -50265,63 +50264,6 @@ var RigidBody = function (_Base) {
     value: function setPosition(x, y) {
       this.bodyDef.get_position().set_x(x);
       this.bodyDef.get_position().set_y(y);
-    }
-
-    /**
-     * Sets the angle (rotation) of the body.
-     * @param {number} angle - Rotation angle in radians
-     */
-
-  }, {
-    key: 'setRotation',
-    value: function setRotation(angle) {
-      this.bodyDef.angle.set_angle(angle);
-    }
-
-    /**
-     * Prevents the collision to be resolved by Box2D, but retains collision information.
-     * @param {boolean} isSensor
-     */
-
-  }, {
-    key: 'setSensor',
-    value: function setSensor(isSensor) {
-      this.materialDef.set_isSensor(isSensor);
-    }
-
-    /**
-     * Prevents the collision to be resolved through other objects.
-     * <b>Expensive! Use with care.</b>
-     * @param {boolean} preventTunneling
-     */
-
-  }, {
-    key: 'setPreventTunneling',
-    value: function setPreventTunneling(preventTunneling) {
-      this.bodyDef.set_bullet(preventTunneling);
-    }
-
-    /**
-     * Prevents or allows the rotation on this RigidBody.
-     * @param {boolean} allowRotation
-     */
-
-  }, {
-    key: 'setAllowRotation',
-    value: function setAllowRotation(allowRotation) {
-      this.bodyDef.set_fixedRotation(!allowRotation);
-    }
-
-    /**
-     * Allows to store a data (in the means of a pointer) of an object to work with the internal memory of the Box2D.
-     * It is (kinda) bugged on emscripten port, but can be {@link https://github.com/kripken/box2d.js/issues/35|worked around}.
-     * @param {Object} userData
-     */
-
-  }, {
-    key: 'setUserData',
-    value: function setUserData(userData) {
-      this.materialDef.set_userData(userData);
     }
 
     /**
